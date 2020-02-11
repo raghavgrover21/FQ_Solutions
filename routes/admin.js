c26cb457fee21b72bf2a6162eea7cda48c1b04ed
raghav <raghav.grover2121@gmail.com>
2020-02-12 01:36:00 +0530

    added routes-> addevent, added functionality-> hashing password

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
 //Models
 const User = require("../models/User");
@@ -11,22 +12,28 @@ const checkRole = require("../services/checkRole");
 //used async and await instead of callbacks and promises to maintain a clean code
 
 //Add a user
-router.post("/add", checkToken, async (req, res) => {
-  if (checkRole(req.decoded.role)) {
+router.post("/add/user", checkToken, async (req, res) => {
+  //Encrpyt the users password
+  const hashPassword = bcrypt.hashSync(req.body.password, 10);
+  if (!checkRole(req.decoded.role)) {
     return res.json({ status: false, message: "access denied" });
   }
   const user = {
-    userName: req.body.username,
+    name: req.body.userName,
     mobile: req.body.mobile,
     role: req.body.role,
-    password: req.body.password
+    password: hashPassword,
+    is_active: req.body.isActive,
+    updated_at: new Date().getTime()
   };
 
   try {
     const newUser = await User.create(user);
-    if (newUser) {
-      return res.json({ status: true, message: "user created successfully" });
-    }
+    return res.json({
+      status: true,
+      message: "user created successfully",
+      newUser
+    });
   } catch (err) {
     console.log(err);
     return res.json({ status: false, message: "something went wrong" });
@@ -34,18 +41,49 @@ router.post("/add", checkToken, async (req, res) => {
 });
 
 //Remove a user
-router.post("/remove", checkToken, async (req, res) => {
-  if (checkRole(req.decoded.role)) {
+router.post("/remove/user", checkToken, async (req, res) => {
+  console.log(req.decoded);
+  if (!checkRole(req.decoded.role)) {
     return res.json({ status: false, message: "access denied" });
   }
   const mobile = req.body.mobile;
-  const removedUser = await User.remove({ mobile });
-  if (removedUser) {
+  try {
+    const removedUser = await User.findOneAndUpdate(
+      { mobile },
+      { is_active: false },
+      { new: true }
+    );
+    if (removedUser) {
+      return res.json({
+        status: true,
+        removedUser,
+        message: " User removed successfully"
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.json({ status: false, message: "something went wrong" });
   }
 });
 
 //Add event
-router.post("/add/event", (req, res) => {
+router.post("/add/event", checkToken, async (req, res) => {
+  const eventDetails = {
+    organizer_id: req.body.organizerid,
+    event_name: req.body.eventname,
+    organizer_name: req.body.organizername,
+    sponsors: req.body.sponsors,
+    is_active: req.body.isActive
+  };
+  try {
+    const addEvent = await Event.create(eventDetails);
+    res.json({ status: true, message: "event added successfully", addEvent });
+  } catch (err) {
+    console.log(err);
+    res.json({ status: false, message: "something went wrong" });
+  }
 });
+
+//Remove an event
 
 module.exports = router;
