cf023cac1fadb65d31fbd62af9856e3713c4fbdb
raghav <raghav.grover2121@gmail.com>
2020-02-12 01:37:12 +0530

    added validations and else statement if the user is not found

--- routes/login.js
+++ routes/login.js
@@ -7,12 +7,25 @@ const User = require("../models/User");
 
 //Services
 const generateToken = require("../services/generateToken");
+const { validateLogin } = require("../services/validators");
+const sanitizeError = require("../services/sanitizeValidation");
 
 //Login route
 router.post("/", async (req, res) => {
+  //Validations
+  const { error } = validateLogin(req.body);
+  if (error) {
+    let message = error.details[0].message;
+    let invalidType = sanitizeError(message);
+    return res.json({
+      status: false,
+      message: `${invalidType} is invalid`
+    });
+  }
+
   const mobile = req.body.mobile;
   const password = req.body.password;
-  const userDetails = await User.findOne({mobile});
+  const userDetails = await User.findOne({ mobile });
   if (!userDetails) {
     return res.json({ status: true, message: "user not found" });
   } else {
@@ -24,6 +37,8 @@ router.post("/", async (req, res) => {
         userDetails.role
       );
       return res.json({ status: true, message: "user logged in", token });
+    } else {
+      res.json({ status: true, message: "invalid username or password" });
     }
   }
 });
