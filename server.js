3474c8d8771add58b4a517acf7e3315b92813b78
raghav <raghav.grover2121@gmail.com>
2020-02-11 23:09:24 +0530

    added database connections and routes

--- server.js
+++ server.js
@@ -1,5 +1,43 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+// External libraries
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+// Cross origin so that this application communicate with another port on same pc or another pc.
+const cors = require("cors");
+
+//Routes
+const admin = require("./routes/admin");
+const signup = require("./routes/signup");
+const login = require("./routes/login");
+
+//Initializing express
 const app = express();
 app.use(bodyParser.json());
+
+//connect to mongodb
+mongoose.connect("mongodb://localhost:27017/FQsol", { useNewUrlParser: true });
+
+//Server port number
+const port = process.env.port || 80;
+app.listen(port, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Server running on port ${port}`);
+  }
+});
+
+//Test route
+app.post("/", (req, res) => {
+  res.send("working");
+});
+
+//Admin Route
+app.use("/admin", admin);
+
+//Signup Route
+app.use("/signup",signup);
+
+//login Route
+app.use("/login",login);
