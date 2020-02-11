970e0b6425c9d7ac104548c7b48725e204dd0619
raghav <raghav.grover2121@gmail.com>
2020-02-12 01:41:54 +0530

    added validations for signup and login

---
+++ services/validators.js
@@ -0,0 +1,26 @@
+//Validations file as per each API
+//Note - Token based API are not required to be validated
+const Joi = require("@hapi/joi");
+
+//validate signup
+const validateSignUp = data => {
+  const schema = Joi.object({
+    mobile: Joi.string().required(),
+    name: Joi.string().required(),
+    role: Joi.string().required(),
+    is_active: Joi.string().required()
+  }).strict();
+  return schema.validate(data);
+};
+
+//Validate login
+const validateLogin = data => {
+  const schema = Joi.object({
+    mobile: Joi.string().required(),
+    password: Joi.string().required()
+  }).strict();
+  return schema.validate(data);
+};
+
+module.exports.validateSignUp = validateSignUp;
+module.exports.validateLogin = validateLogin;
