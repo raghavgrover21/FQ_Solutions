9c45bda2f9bb0edbf3260676dbd55dd56196b4c7
raghav <raghav.grover2121@gmail.com>
2020-02-11 23:06:02 +0530

    can add a user, event

---
+++ routes/admin.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+
+//Models
+const User = require("../models/User");
+const Event = require("../models/Event");
+
+//Services
+const checkToken = require("../services/checkToken");
+const checkRole = require("../services/checkRole");
+//used async and await instead of callbacks and promises to maintain a clean code
+
+//Add a user
+router.post("/add", checkToken, async (req, res) => {
+  if (checkRole(req.decoded.role)) {
+    return res.json({ status: false, message: "access denied" });
+  }
+  const user = {
+    userName: req.body.username,
+    mobile: req.body.mobile,
+    role: req.body.role,
+    password: req.body.password
+  };
+
+  try {
+    const newUser = await User.create(user);
+    if (newUser) {
+      return res.json({ status: true, message: "user created successfully" });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.json({ status: false, message: "something went wrong" });
+  }
+});
+
+//Remove a user
+router.post("/remove", checkToken, async (req, res) => {
+  if (checkRole(req.decoded.role)) {
+    return res.json({ status: false, message: "access denied" });
+  }
+  const mobile = req.body.mobile;
+  const removedUser = await User.remove({ mobile });
+  if (removedUser) {
+  }
+});
+
+//Add event
+router.post("/add/event", (req, res) => {
+});
+
+module.exports = router;
