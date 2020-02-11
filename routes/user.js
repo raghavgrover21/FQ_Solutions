225bfa24d11cf8f78ac1232a835400f336263b2a
raghav <raghav.grover2121@gmail.com>
2020-02-12 01:39:32 +0530

    models changes reflect here and added /add/event(add event to wishlist)

--- routes/user.js
+++ routes/user.js
@@ -2,15 +2,17 @@ const express = require("express");
 const router = express.Router();
 //lodash - sanatize data
 const _ = require("lodash");
+
 //Models
-const User = require("../models/User");
+// const User = require("../models/User");
 const Event = require("../models/Event");
+const UserEvents = require("../models/UserEvents");
 
 //Services
 const checkToken = require("../services/checkToken");
 
 //Fetch all events
-router.post("/", checkToken, async (req, res) => {
+router.post("/get/allevents", checkToken, async (req, res) => {
   try {
     //limit for pagination
     const allEvents = await Event.find().limit(10);
@@ -18,9 +20,16 @@ router.post("/", checkToken, async (req, res) => {
     //Sanitize allEvents data
     if (allEvents.length > 0) {
       let events = _.map(allEvents, obj => {
-        return _.pick(obj, ["name", "organizer", "sponsors"]);
+        return _.pick(obj, [
+          "event_name",
+          "organizer",
+          "organizer_name",
+          "sponsors"
+        ]);
       });
       res.json({ status: true, events });
+    } else {
+      res.json({ status: true, message: "no events available as of now" });
     }
   } catch (err) {
     console.log(err);
@@ -28,4 +37,22 @@ router.post("/", checkToken, async (req, res) => {
   }
 });
 
+router.post("/add/event", checkToken, async (req, res) => {
+  const userEvents = {
+    mobile: req.decoded.mobile,
+    event_name: req.body.eventname,
+    organizer_name: req.body.organizername
+  };
+  try {
+    const addEvent = await UserEvents.create(userEvents);
+    res.json({
+      status: true,
+      message: "event successfully added to your wishlist",
+      addEvent
+    });
+  } catch (err) {
+    console.log(err);
+    res.json({ status: false, message: "something went wrong" });
+  }
+});
 module.exports = router;
